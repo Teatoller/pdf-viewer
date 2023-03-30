a375ecee2a6e9036647deaaf6c32c58bebe03819
steven ennis <steven.otieno1@gmail.com>
2023-03-30 13:38:12 +0300

    ft(components): add components
    - input field
    - button
    - pdf view container
    - styles

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../index.css";
 
 export default function PDFViewer() {
   return (
-    <div className='container' >PDFViewer</div>
-  )
+    <div className="container">
+      <form>
+        <input type="file" className="form-control" />
+        <button type="submit" className="btn btn-primary">
+          View PDF
+        </button>
+      </form>
+      <h2>View PDF</h2>
+      <div className="pdf-container"></div>
+    </div>
+  );
 }
