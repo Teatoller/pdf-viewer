bc652875f94fb4df6fa70c9ca5906a7c94486d21
steven ennis <steven.otieno1@gmail.com>
2023-03-30 13:13:29 +0300

    ch(project setup): project set up

---
+++ src/components/PDFViewer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function PDFViewer() {
+  return (
+    <div className='container' >PDFViewer</div>
+  )
+}
